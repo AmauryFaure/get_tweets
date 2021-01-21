c51fc8d1d07a7e7b8612cbc57dc21a8d5f46461b
amaury2650@gmail.com <VCLNTBmathsp2971>
2021-01-21 08:49:06 +0100

    Initial commit

---
+++ get_tweets.py
@@ -0,0 +1,47 @@
+import tweepy
+import csv
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv("credentials.env")
+auth = tweepy.AppAuthHandler(os.getenv("consumer_key"), os.getenv("consumer_secret"))
+api = tweepy.API(auth)
+
+
+def get_tweet_text(tweet):
+    """This function returns the full tweet content for a tweet object of tweepy.com
+
+    This function receives a tweet objects and returns the full text content of the tweet.
+
+    :param tweet: A tweet received from twitter
+    :return: The text of this tweet as a string
+    """
+    if hasattr(tweet, "retweeted_status"):  # Check if Retweet
+        try:
+            content = tweet.retweeted_status.extended_tweet["full_text"]
+        except AttributeError:
+            content = tweet.retweeted_status.full_text
+    else:
+        try:
+            content = tweet.extended_tweet["full_text"]
+        except AttributeError:
+            content = tweet.full_text
+    return content
+
+#Define export file
+file = open('test.csv', 'w', newline='')
+writer = csv.writer(file)
+#To look for all stop words use this portion
+
+with open("stopwords.csv", newline='') as words_file:
+    reader = csv.reader(words_file)
+    for row in reader:
+        search_term = row
+        for tweet in tweepy.Cursor(api.search, q=search_term, lang="fr", count="20", tweet_mode="extended").items(15):
+            writer.writerow([get_tweet_text(tweet), search_term])
+
+#To search for a special term :
+# search_term="bonjour"
+# for tweet in tweepy.Cursor(api.search, q=search_term, lang="fr", count="20", tweet_mode="extended").items(10):
+#     writer.writerow([get_tweet_text(tweet), search_term])
